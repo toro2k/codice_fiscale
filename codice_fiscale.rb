bebc10951dde849a5199a3906da62f3f5620c49a
atorelli <andrea.torelli@ixenia.it>
2013-04-17 10:55:39 +0200

    .

--- codice_fiscale.rb
+++ codice_fiscale.rb
@@ -9,45 +9,45 @@
 # CodiceFiscale.new('RSSMRA70A01H501T').valid? # => false
 class CodiceFiscale
 
-	LETTERS = Hash[('A'..'Z').each_with_index.to_a].freeze
-	DIGITS = Hash[('0'..'9').each_with_index.to_a].freeze
-	CF_TABLE = {
-		:even => {
-			'0' => 1, '1' => 0, '2' => 5, '3' => 7, '4' => 9, '5' => 13,
-			'6' => 15, '7' => 17, '8' => 19, '9' => 21, 'A' => 1, 'B' => 0,
-			'C' => 5, 'D' => 7, 'E' => 9, 'F' => 13, 'G' => 15, 'H' => 17,
-			'I' => 19, 'J' => 21, 'K' => 2, 'L' => 4, 'M' => 18, 'N' => 20,
-			'O' => 11, 'P' => 3, 'Q' => 6, 'R' => 8, 'S' => 12, 'T' => 14,
-			'U' => 16, 'V' => 10, 'W' => 22, 'X' => 25, 'Y' => 24, 'Z' => 23
-		},
-		:odd => LETTERS.merge(DIGITS)
-	}.freeze
+  LETTERS = Hash[('A'..'Z').each_with_index.to_a].freeze
+  DIGITS = Hash[('0'..'9').each_with_index.to_a].freeze
+  CF_TABLE = {
+    :even => {
+      '0' => 1, '1' => 0, '2' => 5, '3' => 7, '4' => 9, '5' => 13,
+      '6' => 15, '7' => 17, '8' => 19, '9' => 21, 'A' => 1, 'B' => 0,
+      'C' => 5, 'D' => 7, 'E' => 9, 'F' => 13, 'G' => 15, 'H' => 17,
+      'I' => 19, 'J' => 21, 'K' => 2, 'L' => 4, 'M' => 18, 'N' => 20,
+      'O' => 11, 'P' => 3, 'Q' => 6, 'R' => 8, 'S' => 12, 'T' => 14,
+      'U' => 16, 'V' => 10, 'W' => 22, 'X' => 25, 'Y' => 24, 'Z' => 23
+    },
+    :odd => LETTERS.merge(DIGITS)
+  }.freeze
 
-	def initialize(string)
-		string.upcase!
-		fail string unless string =~ /\A[A-Z0-9]{16}\Z/
-		*@payload, @control = string.each_char.to_a
-	end
+  def initialize(string)
+    string.upcase!
+    fail ArgumentError.new(string) unless string =~ /\A[A-Z0-9]{16}\Z/
+    *@payload, @control = string.each_char.to_a
+  end
 
-	def valid?
-		checksum == @control
-	end
+  def valid?
+    checksum == @control
+  end
 
-	def checksum
-		sum = @payload.each_with_index.reduce(0) do |sum, c|
-			char, index = c
-			if index.even?
-				sum += CF_TABLE[:even][char]
-			else
-				sum += CF_TABLE[:odd][char]
-			end
-			next sum
-		end
-		return LETTERS.rassoc(sum % 26)[0]
-	end
+  def checksum
+    sum = @payload.each_with_index.reduce(0) do |sum, c|
+      char, index = c
+      if index.even?
+        sum += CF_TABLE[:even][char]
+      else
+        sum += CF_TABLE[:odd][char]
+      end
+      next sum
+    end
+    return LETTERS.rassoc(sum % 26)[0]
+  end
 
-	def to_s
-		"<CF #{@payload.join('')}#{@control}>"
-	end
+  def to_s
+    "<CF #{@payload.join('')}#{@control}>"
+  end
 
 end
--- partita_iva.rb
+++ partita_iva.rb
@@ -5,33 +5,33 @@
 # DA VERIFICARE!!!
 class PartitaIva
 
-	def initialize(string)
-		fail string unless string =~ /\A[0-9]{11}\Z/
-		*@payload, @control = string.each_char.map(&:to_i)
-	end
+  def initialize(string)
+    fail ArgumentError.new(string) unless string =~ /\A[0-9]{11}\Z/
+    *@payload, @control = string.each_char.map(&:to_i)
+  end
 
-	def valid?
-		checksum == @control
-	end
+  def valid?
+    checksum == @control
+  end
 
-	def checksum
-		sum = @payload.each_with_index.reduce(0) do |sum, c|
-			digit, index = c
-			if index.even?
-				sum += digit
-			else
-				add = digit * 2
-				add -= 9 while add > 9
-				sum += add
-			end
-			next sum
-		end
-		rest = sum % 10
-		return rest.zero? ? 0 : 10 - rest
-	end
+  def checksum
+    sum = @payload.each_with_index.reduce(0) do |sum, c|
+      digit, index = c
+      if index.even?
+        sum += digit
+      else
+        add = digit * 2
+        add -= 9 while add > 9
+        sum += add
+      end
+      next sum
+    end
+    rest = sum % 10
+    return rest.zero? ? 0 : 10 - rest
+  end
 
-	def to_s
-		"<PI #{@payload.join('')}#{@control}>"
-	end
+  def to_s
+    "<PI #{@payload.join('')}#{@control}>"
+  end
 
 end
